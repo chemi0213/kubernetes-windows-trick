f9118a340fb9a34e147e4ac8927e8af82d56525c
Phil Huang <phil.huang@microsoft.com>
2024-08-29 23:21:38 +0800

    xx

--- install-stakater.sh
+++ install-stakater.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 helm repo add stakater https://stakater.github.io/stakater-charts
 helm repo update
-helm install stakater/reloader
 helm install stakater/reloader --set reloader.watchGlobally=false --namespace ns-windows-management
  --generate-name
